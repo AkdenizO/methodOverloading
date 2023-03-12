5984f504d26d97f827d7bd4612242829793cf01d
oezal <akdenizozal@gmail.com>
2023-03-12 18:56:05 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        VierRechenarten vierRechenarten = new VierRechenarten();
+
+        System.out.println(vierRechenarten.addition(15, 10));
+
+        System.out.println(vierRechenarten.addition(15, 20, 30));
+
+
+    }
+}
---
+++ src/VierRechenarten.java
@@ -0,0 +1,20 @@
+public class VierRechenarten {
+    public int addition(int zahl1, int zahl2){
+        return zahl1 + zahl2;
+    }
+
+    public int subraktion(int zahl1, int zahl2){
+        return zahl1 - zahl2;
+    }
+
+    public int multiplikation(int zahl1, int zahl2){
+        return zahl1 * zahl2;
+    }
+
+    public double division(int zahl1, int zahl2){
+        return zahl1 / zahl2;
+    }
+    public int addition(int zahl1, int zahl2, int zahl3){
+        return zahl1 + zahl2 + zahl3;
+    }
+}
